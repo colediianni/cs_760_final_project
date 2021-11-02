1e8fdefb5f520c16c4248ec14d9f12809288639b
colediianni <cwdiianni@gmail.com>
2021-11-02 10:58:47 -0500

    Added GAN generator

---
+++ Generate_Images.py
@@ -0,0 +1,53 @@
+import numpy
+from numpy import expand_dims
+from numpy import zeros
+from numpy import ones
+from numpy.random import randn
+from numpy.random import randint
+from tensorflow.keras.datasets.fashion_mnist import load_data
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Reshape
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Conv2DTranspose
+from tensorflow.keras.layers import LeakyReLU
+from tensorflow.keras.layers import Dropout
+import tensorflow as tf
+
+# Setting Seed
+numpy.random.seed(seed=123)
+
+# example of loading the generator model and generating images
+from tensorflow.keras.models import load_model
+from numpy.random import randn
+from matplotlib import pyplot
+
+# generate points in latent space as input for the generator
+def generate_latent_points(latent_dim, n_samples):
+	# generate points in the latent space
+	x_input = randn(latent_dim * n_samples)
+	# reshape into a batch of inputs for the network
+	x_input = x_input.reshape(n_samples, latent_dim)
+	return x_input
+
+# create and save a plot of generated images (reversed grayscale)
+def show_plot(examples, n):
+	# plot images
+	for i in range(n * n):
+		# define subplot
+		pyplot.subplot(n, n, 1 + i)
+		# turn off axis
+		pyplot.axis('off')
+		# plot raw pixel data
+		pyplot.imshow(examples[i, :, :, 0], cmap='gray_r')
+	pyplot.show()
+
+# load model
+model = load_model('./GAN_models/generator.h5')
+# generate images
+latent_points = generate_latent_points(100, 100)
+# generate images
+X = model.predict(latent_points)
+# plot the result
+show_plot(X, 10)
